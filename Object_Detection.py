9f9ff14bdac51cc34e4acbaa5e8072f719c1284a
Arman Shaikh <armaanshakeelshaikh@gmail.com>
2025-11-21 00:48:08 +0400

    Display frames,auto exposure coded

--- Object_Detection.py
+++ Object_Detection.py
@@ -1,16 +1,23 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
+# Auto exposure on
+cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
 
 while True:
-    ret, img = cap.read()
+    ret, img = cam.read()
     if not ret:
         break
 
     cv2.imshow("Original", img)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Mask Open", maskOpen)
+    cv2.imshow("Mask Close", maskClose)
+
+
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
-cap.release()
+cam.release()
 cv2.destroyAllWindows()
