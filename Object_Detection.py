11580b9643379724b044cfc2d48a5dfafa85e4d0
Arman Shaikh <armaanshakeelshaikh@gmail.com>
2025-11-20 15:10:39 +0400

    Segmentation edits in main Python file

--- Object_Detection.py
+++ Object_Detection.py
@@ -1,13 +1,23 @@
 import cv2
 import numpy as np
+#Define HSV ranges for red color
+lower_red1 = np.array([0, 80, 60])
+upper_red1 = np.array([10, 255, 255]) 
+lower_red2 = np.array([170, 80, 60])
+upper_red2 = np.array([180, 255, 255]) 
 
+# Initialize video capture
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, img = cap.read()
     if not ret:
         break
-
+    imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask1 = cv2.inRange(imghsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(imghsv, lower_red2, upper_red2)
+    mask = mask1 + mask2        
+                
     cv2.imshow("Original", img)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
