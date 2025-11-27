97133a9b3ce45675c7eb7459a5dbd841c8dc34e5
Arman Shaikh <armaanshakeelshaikh@gmail.com>
2025-11-27 20:40:40 +0400

    fix: videocapture code

--- Toggle switch.py
+++ Toggle switch.py
@@ -17,8 +17,10 @@ kernelo = np.ones((5, 5))
 kernelc = np.ones((10, 10)) 
 
 # Initialize video capture
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 # Toggle flags
 show_red = False
